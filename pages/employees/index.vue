c832764b785e3f67fe1b8453fd1b3f8ec6980568
A36498 <A36498@thanglong.edu.vn>
2022-10-12 22:07:10 +0700

    feat(impl/employeess): employess user interface

---
+++ pages/employees/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="employee-page">
+    <layout-title :title="title"></layout-title>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      title: 'Quản lý Người dùng',
+    };
+  },
+};
+</script>
