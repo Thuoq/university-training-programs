c3ebab80d3bd504bcbe3e0bc5454a938e4402d7e
A36498 <A36498@thanglong.edu.vn>
2022-10-12 21:19:03 +0700

    feat(impl/positions): positions user interface

---
+++ pages/positions/-position-dialog.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="position-dialog">
+    <h2 class="title">Thêm mới</h2>
+    <div class="body">
+        <div class="inputgroup">
+          <label class="label">Mã nhóm người dùng</label>
+          <input  class="input" type="text" required />
+        </div>
+        <div class="inputgroup" style="margin-right: 3.5px;">
+          <label class="label">Tên nhóm người dùng</label>
+          <input v-model="name" class="input" type="text" required />
+        </div>
+    </div>
+    <div class="footer">
+      <app-button raised class="btn -delete" @click="onClosed">Huỷ</app-button>
+      <app-button raised class="btn -save" @click="onSubmit">Lưu</app-button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+    };
+  },
+  methods: {
+    onClosed() {
+      this.$emit('closed');
+    },
+    onSubmit() {
+      const payload = this.$data;
+      this.$emit('submit', payload);
+      this.$emit('closed');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.position-dialog {
+  > .title {
+    margin: -19px -24px 0px -24px;
+    padding-top: 13px;
+    padding-bottom: 10px;
+    height: 27px;
+    text-align: center;
+    font-size: 17px;
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 700;
+    line-height: 21px;
+    color: var(--color-back);
+    background: #ebebeb;
+    border-bottom: 1px solid var(--color-back);
+  }
+
+  > .body {
+    text-align: center;
+    margin: 23px 0px 23px 0px;
+    > .inputgroup {
+      position: relative;
+      display: inline-block;
+    }
+    > .inputgroup:not(:last-child) {
+      margin-bottom: 23.13px;
+    }
+  }
+
+  > .body > .inputgroup > .input {
+    background-color: var(--color-white);
+    border: 1px solid black;
+    width: 250px;
+    height: 40px;
+    border-radius: 5px;
+  }
+
+  > .body > .inputgroup > .label {
+    margin-right: 10px;
+    width: 200px;
+    text-align: right;
+    margin-top: 10px;
+    font-size: 16px;
+    font-weight: 400;
+    font-family: 'Inter';
+    color: var(--color-back);
+  }
+
+  .footer {
+    display: flex;
+    justify-content: space-around;
+    align-content: center;
+    margin: 0px 50px 0px 50px;
+  }
+
+  > .footer > .btn {
+    &.-save {
+      --mdc-theme-primary: var(--color-primary);
+    }
+    &.-delete {
+      --mdc-theme-primary: var(--color-gray-base);
+      --mdc-theme-on-primary: var(--color-back);
+    }
+  }
+}
+</style>
