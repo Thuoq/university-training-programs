1abf7ecc60e87db5cd3dd23ffa711f987e7644e5
A36498 <A36498@thanglong.edu.vn>
2022-10-25 15:17:42 +0700

    feat(impl/-edit-faculty): implement edit func faculty page v2

--- models/sections.model.js
+++ models/sections.model.js
@@ -2,6 +2,13 @@ import { apis } from '~/apis';
 
 export const fetchListSections = () => apis.universityAPI.get('sections').json();
 
+export const createSection = (payload) =>
+  apis.universityAPI
+    .post('sections', {
+      json: payload,
+    })
+    .json();
+
 export const updateSection = (payload) =>
   apis.universityAPI
     .put(`sections/${payload.id}`, {
